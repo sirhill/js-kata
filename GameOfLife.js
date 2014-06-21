2529870dbc1449e4974d0504c9c242274634a72b
sirhill <cyril.lapinte@free.fr>
2014-06-21 15:09:58 -0700

    GameOfLife: not correctly tested

--- GameOfLife.js
+++ GameOfLife.js
@@ -19,27 +19,44 @@ function GameOfLife(size) {
    }
 
    this.init = function() {
-      var board = [];
+      var rowF = function(board) { return function(i) { board[i] = []; }; };
+      var caseF = function(board) { return function(i,j) { board[i][j] = GameOfLife.DEAD; }; };
+
       this.iterate(
-        function(i) { board[i] = [] },
-        function(i,j) { board[i][j] = GameOfLife.DEAD; }
+        rowF(this.board),
+        caseF(this.board)
       );
-      this.board = board;
    }
 
-   this.setBoard = function(values) {
+   this.initBoard = function(values) {
       this.init();
       for(var i=0, value; value = values[i]; i++) {
         this.board[value[0]][value[1]] = GameOfLife.LIVING;
       }
    }
 
+   this.getBoard = function() {
+      var boardWithBorder = [];
+      for(var i=0; i<this.size+2; i++) {
+        boardWithBorder[i] = [];
+        for(var j=0;j<this.size+2; j++) {
+           if((i>0 && j>0) && (i<this.size+1 && j<this.size+1)) {
+             boardWithBorder[i][j] = this.board[i-1][j-1];
+           }
+           else {
+             boardWithBorder[i][j] = GameOfLife.BORDER;
+           }
+        }
+      }
+      return boardWithBorder;
+   }
+
    this.display = function(div) {
      div.innerHTML = "";
-     for(var i=0; i<GameOfLife.SIZE; i++) {
+     for(var i=0; i<this.size; i++) {
        var tr = document.createElement("tr");
        div.appendChild(tr);
-       for(var j=0;j<GameOfLife.SIZE; j++) {
+       for(var j=0;j<this.size; j++) {
          var td = document.createElement("td");
          tr.appendChild(td);
          td.innerHTML=this.board[i][j];
@@ -48,24 +65,45 @@ function GameOfLife(size) {
      div.style.cssText="line-height:40%;font-family: 'courier'";
    }
 
-   this.next = function() {
-     var applyRule = this.applyRule;
-     var getCell = this.getCell;
-     var board = this.board;
- 
-     var caseF = function(i,j) {
-       this.board = board;
-       applyRule(getCell(i,j));
+   this.getNeighbourCount = function(i, j) {
+     var count = 0;
+     var iMin = (i==0) ? 0 : -1;
+     var iMax = (i==this.size-1) ? 0: 1;
+     var jMin = (j==0) ? 0 : -1;
+     var jMax = (j==this.size-1) ? 0: 1;
+     //console.log("("+i+", "+j+") " +iMin+", "+iMax+" : "+jMin + ", " + jMax);
+     for(var cI = iMin; cI <= iMax; cI++) {
+       for(var cJ = jMin; cJ <= jMax; cJ++) {
+	  if(!(cI==0 && cJ==0) && this.board[i+cI][j+cJ] == GameOfLife.LIVING) {
+             count++;
+          }
+       } 
      }
- 
-     this.iterate(
-       function(i) { },
-       caseF 
-     );
+     return count;
+   }
+
+   this.next = function() {
+     var nextBoard = [];
+     for(var i=0; i<this.size; i++) {
+       nextBoard[i] = [];
+       for(var j=0;j<this.size; j++) {
+         var neighbourCount = this.getNeighbourCount(i,j);
+
+	 if(this.board[i][j] == GameOfLife.LIVING && (neighbourCount < 2 || neighbourCount > 3)) {
+           nextBoard[i][j] = GameOfLife.DEAD; 
+         }
+         else if(this.board[i][j] == GameOfLife.DEAD && neighbourCount == 3) {
+           nextBoard[i][j] = GameOfLife.LIVING;
+         }
+         else {
+           nextBoard[i][j] = this.board[i][j];
+         }
+       }
+     } 
+     this.board = nextBoard;
    }
 
    this.getCell = function(i,j) {
-      console.log(this.board);
       return this.board[i][j];
    }
 
@@ -77,7 +115,8 @@ function GameOfLife(size) {
 GameOfLife.SIZE = 10;
 GameOfLife.DEAD = "&nbsp;";
 GameOfLife.LIVING = "o";
+GameOfLife.BORDER = "x";
 
 GameOfLife.instance = new GameOfLife(GameOfLife.SIZE);
-GameOfLife.instance.setBoard( [ [ 4, 4 ], [ 4, 5 ], [ 5, 4 ], [ 5, 5 ] ] );
+GameOfLife.instance.initBoard( [ [ 4, 5 ], [ 5, 4 ], [ 5, 5 ], [ 5, 6 ], [ 6, 5 ] ] );
 
--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -1,4 +1,7 @@
 describe("GameOfLife next generation", function() {
+   var b = GameOfLife.BORDER;
+   var d = GameOfLife.DEAD;
+   var l = GameOfLife.LIVING;
 
    it("init()", function() {
       var gameOfLife = new GameOfLife(1);
@@ -13,29 +16,61 @@ describe("GameOfLife next generation", function() {
       expect(gameOfLife.board).toEqual([[GameOfLife.DEAD]]);
    });
 
-   it("setBoard()", function() {
+   it("initBoard()", function() {
       var gameOfLife = new GameOfLife(1);
-      gameOfLife.setBoard([ [ 0, 0 ] ]);
+      gameOfLife.init();
+      gameOfLife.initBoard([ [ 0, 0 ] ]);
       expect(gameOfLife.board[0][0]).toBe(GameOfLife.LIVING);
    });
 
    it("next()", function() {
       var gameOfLife = new GameOfLife(1);
-      gameOfLife.setBoard([ [ 0, 0 ] ]);
+      gameOfLife.init();
+      expect(gameOfLife.getBoard()).toEqual([ [ b, b, b ], [ b, d, b ], [ b, b, b] ] );
+   });
+
+   it("getCell() when size is 1", function() {
+      var gameOfLife = new GameOfLife(1);
+      gameOfLife.init();
+      gameOfLife.initBoard([[0,0]]);
+      expect(gameOfLife.getBoard()).toEqual([ [ b, b, b ], [ b, l, b ], [ b, b, b] ] );
+   });
+
+   it("getNeightbourCount", function() {
+      var gameOfLife = new GameOfLife(3);
+      gameOfLife.init();
+      expect(gameOfLife.getNeighbourCount(0,0)).toEqual(0);
+      expect(gameOfLife.getNeighbourCount(0,2)).toEqual(0);
+      expect(gameOfLife.getNeighbourCount(2,0)).toEqual(0);
+      expect(gameOfLife.getNeighbourCount(2,2)).toEqual(0);
+
+      gameOfLife.initBoard([[1,1]]);
+      expect(gameOfLife.getBoard()).toEqual([ [ b, b, b, b, b ], [ b, d, d, d, b ], [ b, d, l, d, b ], [ b, d, d, d, b ], [ b, b, b, b, b ] ]);
+      expect(gameOfLife.getNeighbourCount(0,0)).toEqual(1);
+      expect(gameOfLife.getNeighbourCount(0,2)).toEqual(1);
+      expect(gameOfLife.getNeighbourCount(2,0)).toEqual(1);
+      expect(gameOfLife.getNeighbourCount(2,2)).toEqual(1);
+
+      gameOfLife.initBoard([[1,1],[2,1]]);
+      expect(gameOfLife.getNeighbourCount(0,0)).toEqual(1);
+      expect(gameOfLife.getNeighbourCount(0,2)).toEqual(1);
+      expect(gameOfLife.getNeighbourCount(2,0)).toEqual(2);
+      expect(gameOfLife.getNeighbourCount(2,2)).toEqual(2);
+   });
+
+   it("underpopulation", function() {
+      var gameOfLife = new GameOfLife(3);
+      gameOfLife.initBoard([[1,1]]);
       gameOfLife.next();
+      expect(gameOfLife.getBoard()).toEqual([ [ b, b, b, b, b ], [ b, d, d, d, b ], [ b, d, d, d, b ], [ b, d, d, d, b ], [ b, b, b, b, b ] ]); 
    });
 
-   it("getCell()", function() {
-      var gameOfLife = new GameOfLife(1);
-      gameOfLife.init();
-      gameOfLife.getCell(0,0);
-      expect(gameOfLife.getCell(0,0)).toBe(GameOfLife.DEAD);
+   it("overpopulation", function() {
+      var gameOfLife = new GameOfLife(3);
+      gameOfLife.initBoard([[0,0],[0,1],[1,0],[1,1],[2,0],[2,1]]);
+      gameOfLife.next();
+      expect(gameOfLife.getBoard()).toEqual([ [ b, b, b, b, b ], [ b, l, l, d, b ], [ b, d, d, l, b ], [ b, l, l, d, b ], [ b, b, b, b, b ] ]);
    });
 
-   it("applyRule()", function() {
-      var gameOfLife = new GameOfLife(1);
-      gameOfLife.init();
-      expect(gameOfLife.applyRule(0)).toBe(0); 
-   });
 });
 
